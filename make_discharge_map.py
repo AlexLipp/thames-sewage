45845bafdda132ef67f92be650eddbc5245e4bc4
Alex Lipp <alex@lipp.org.uk>
2023-02-05 15:57:27 +0000

    Automatically save geojsons and plots to timestamped file

--- make_discharge_map.py
+++ make_discharge_map.py
@@ -1,5 +1,6 @@
 import autocatchments as ac
 from datetime import datetime
+import matplotlib.pyplot as plt
 import sewage as swg
 
 """Fetches real-time discharge data from Thames Water and visualises downstream impacts in real time"""
@@ -20,14 +21,19 @@ mg = ac.toolkit.load_d8("input_dir/thames_d8.nc")
 # )
 
 print("### Getting sewage discharge data ###")
-sewage_df = swg.get_thames_data()
+sewage_df = swg.get_current_discharge_status()
 # datetime object containing current date and time
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+dt_string_file = now.strftime("%y%m%d_%H%M%S")
+
 x, y, z = swg.calc_downstream_polluted_nodes(sewage_df, mg)
-swg.plot_sewage_map(downstream_xyz=(x, y, z), grid=mg, sewage_df=sewage_df, title=dt_string)
 long, lat = swg.BNG_to_WGS84_points(x, y)
+
 print("### Plotting outputs ###")
+swg.plot_sewage_map(downstream_xyz=(x, y, z), grid=mg, sewage_df=sewage_df, title=dt_string)
+plt.savefig("output_dir/plots/" + dt_string_file + ".png")
+plt.show()
 print("### Saving outputs ###")
 out_geojson = swg.xyz_to_geojson(long, lat, z, label="upstream_sources")
-swg.save_geojson(out_geojson, "upstream_sources.geojson")
+swg.save_geojson(out_geojson, "output_dir/geojsons/" + dt_string_file + ".geojson")
