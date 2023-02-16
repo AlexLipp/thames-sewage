5fc5861f97ddf942886737aee13fcd6629581df4
Alex Lipp <alex@lipp.org.uk>
2023-02-16 23:11:27 +0000

    Fix double processing of json outputs

--- get_all_discharge_events.py
+++ get_all_discharge_events.py
@@ -3,5 +3,5 @@ import sewage as swg
 alerts = swg.get_all_discharge_alerts()
 alerts.sort_values(by="DateTime", ascending=True, inplace=True)
 events_df = swg.alerts_to_events_df(alerts) 
-events_json = events_df.to_json()
-swg.save_json(events_json,"output_dir/discharge_events.json")
+events_dict = events_df.to_dict()
+swg.save_json(events_dict,"output_dir/discharge_events.json")
