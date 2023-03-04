8312ec84a4f6a2bf2c9daaced2bfc5a76843d0b7
Alex Lipp <alex@lipp.org.uk>
2023-03-04 18:01:09 +0000

    Update gitignore

--- input_dir/process_hydr_data.sh
+++ input_dir/process_hydr_data.sh
@@ -1,2 +1,3 @@
 rgn=-R320000/620000/80000/280000
-gmt grdcut ~/Documents/large_data/UK_hydro_map/uk_d8.nc $rgn -Gthames_d8.nc
+# gmt grdcut ~/Documents/large_data/UK_hydro_map/uk_d8.nc $rgn -Gthames_d8.nc
+gmt grdcut ~/Documents/large_data/UK_hydro_map/ihdtm-2016_4881828/14jan16_HGHT_0_000_000_700_1300.asc $rgn -Gthames_elev.nc
