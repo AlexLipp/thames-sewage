cb613a04196916774f8edbf35d60cee5560918ab
Alex Lipp <alex@lipp.org.uk>
2024-07-23 10:47:22 +0100

    Automatically create info geojson which contains more info about discharge impact dstream

--- update.py
+++ update.py
@@ -9,7 +9,7 @@ from aux_funcs import (
     empty_s3_folder,
     upload_file_to_s3,
     write_timestamp,
-    project_geojson_BNG_WGS84,
+    project_featurecollection_BNG_WGS84,
 )
 
 from poopy.companies import ThamesWater
@@ -32,14 +32,18 @@ LOCAL_GEOJSON_DIR = LOCAL_OUTPUT_DIR + "geojsons/"
 LOCAL_HISTORICAL_DATA_DIR = LOCAL_OUTPUT_DIR + "discharges_to_date/"
 # AWS directory to save current outputs to
 AWS_NOW_DIR = "now/"
+# AWS directory to save current info outputs to
+AWS_INFO_NOW_DIR = "info_now/"
 # AWS directory to save historical outputs to
 AWS_HISTORICAL_DIR = "discharges_to_date/"
 # AWS directory to save long-term outputs to
 AWS_PAST_DIR = "past/"
 # Name of the timestamp file to upload (locally + in AWS)
 TIMESTAMP_FILENAME = "timestamp.txt"
-# Name of the geojson file in the AWS bucket for current discharges
+# Name of the geojson files in the AWS bucket for current discharges
 AWS_GEOJSON_FILENAME = "now.geojson"
+# Nam of the geojson files in the AWS bucket for information on current discharges
+AWS_INFO_GEOJSON_FILENAME = "info_now.geojson"
 # Name of the json file in the AWS bucket for historical discharges
 AWS_JSON_FILENAME = "up_to_now.json"
 
@@ -76,6 +80,36 @@ def upload_downstream_impact_files_to_s3(
         object_name=AWS_NOW_DIR + TIMESTAMP_FILENAME,
         profile_name=PROFILE_NAME,
     )
+
+
+def upload_downstream_impact_info_files_to_s3(
+    info_geojson_file_path: str, timestamp: str
+) -> None:
+    """Uploads the downstream impact info files to the ThamesSewage AWS bucket.
+    These info files contain more specific information about the discharge impact."""
+    # Empty the 'now' folder
+    empty_s3_folder(
+        bucket_name=BUCKET_NAME, folder_name=AWS_INFO_NOW_DIR, profile_name=PROFILE_NAME
+    )
+    # Upload file to current 'now' output and also the long-term storage 'past' folder
+    upload_file_to_s3(
+        file_path=LOCAL_GEOJSON_DIR + info_geojson_file_path,
+        bucket_name=BUCKET_NAME,
+        object_name=AWS_INFO_NOW_DIR + AWS_INFO_GEOJSON_FILENAME,
+        profile_name=PROFILE_NAME,
+    )
+    # Add timestamp file to info_now folder
+    write_timestamp(
+        datetime_string=timestamp,
+        timestamp_filename=LOCAL_OUTPUT_DIR + TIMESTAMP_FILENAME,
+    )
+    upload_file_to_s3(
+        file_path=LOCAL_OUTPUT_DIR + TIMESTAMP_FILENAME,
+        bucket_name=BUCKET_NAME,
+        object_name=AWS_INFO_NOW_DIR + TIMESTAMP_FILENAME,
+        profile_name=PROFILE_NAME,
+    )
+
 
 def delete_historical_data_files_from_s3() -> None:
     """Deletes the historical data files from the ThamesSewage AWS bucket"""
@@ -120,15 +154,15 @@ def main():
     geojson_file_name = now.strftime("%y%m%d_%H%M%S.geojson")
     tw = ThamesWater(tw_clientID, tw_clientSecret)
 
-    print("Calculating current downstream discharge information")
+    print("Calculating current downstream discharge extent...")
     geojson = tw.get_downstream_geojson(include_recent_discharges=True)
-    geojson = project_geojson_BNG_WGS84(geojson)  # Project to WGS84 from BNG
     # Save geojson to local directory
 
     # For legacy reasons we need to wrap the geojson in a FeatureCollection...
     feature_coll = FeatureCollection(
         [Feature(geometry=geojson, type="MultiLineString")]
     )
+    feature_coll = project_featurecollection_BNG_WGS84(feature_coll)
 
     with open(LOCAL_GEOJSON_DIR + geojson_file_name, "w") as f:
         json.dump(feature_coll, f)
@@ -137,19 +171,30 @@ def main():
         geojson_file_name, now.isoformat(timespec="seconds")
     )
 
-    print("Fetching historical discharge information")
-    # We do this first so that if the Thames Water API fails we aren't left with out of date data
-    json_file_name = now.strftime("%y%m%d_%H%M%S.json")
-    tw.set_all_histories()
-    df = tw.history_to_discharge_df()
-    # Fill in missing stop times (for ongoing discharges) with now for consistency with www.sewagemap.com legacy inputs
-    df["StopDateTime"] = df["StopDateTime"].fillna(datetime.now())
-    df.to_json(LOCAL_HISTORICAL_DATA_DIR + json_file_name)
-    print("Uploading outputs to AWS bucket")    
-    delete_historical_data_files_from_s3()
-    upload_historical_data_files_to_s3(
-        json_file_name, now.isoformat(timespec="seconds")
+    print("Calculating current downstream discharge information...")
+    info_geojson = tw.get_downstream_info_geojson(include_recent_discharges=True)
+    info_geojson = project_featurecollection_BNG_WGS84(info_geojson)
+    info_geojson_file_name = now.strftime("%y%m%d_%H%M%S_info.geojson")
+    with open(LOCAL_GEOJSON_DIR + info_geojson_file_name, "w") as f:
+        json.dump(info_geojson, f)
+    print("Uploading outputs to AWS bucket")
+    upload_downstream_impact_info_files_to_s3(
+        info_geojson_file_name, now.isoformat(timespec="seconds")
     )
+
+    # print("Fetching historical discharge information...")
+    # # We do this first so that if the Thames Water API fails we aren't left with out of date data
+    # json_file_name = now.strftime("%y%m%d_%H%M%S.json")
+    # tw.set_all_histories()
+    # df = tw.history_to_discharge_df()
+    # # Fill in missing stop times (for ongoing discharges) with now for consistency with www.sewagemap.com legacy inputs
+    # df["StopDateTime"] = df["StopDateTime"].fillna(datetime.now())
+    # df.to_json(LOCAL_HISTORICAL_DATA_DIR + json_file_name)
+    # print("Uploading outputs to AWS bucket")
+    # delete_historical_data_files_from_s3()
+    # upload_historical_data_files_to_s3(
+    #     json_file_name, now.isoformat(timespec="seconds")
+    # )
 
     print("Finished @", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
